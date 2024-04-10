9d4a62fd53f0784c3218d334f8bb6054d44560bf
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2024-04-10 22:12:31 +0900

    ノートアプリを作成

---
+++ noteapp-with-react/src/App.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+import uuid from 'react-uuid';
+
+function App() {
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
+  const [activeNote, setActiveNote] = useState(null);
+
+  useEffect(() => {
+    // ローカルストレージにノートを保存する
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes])
+
+  useEffect(() => {
+    if (notes.length > 0) {
+      setActiveNote(notes[0].id);
+    } else {
+      setActiveNote(null);
+    }
+  }, []);
+
+  const onAddNote = () => {
+    console.log('新しくノートが追加');
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "",
+      modDate: Date.now()
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  }
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  }
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote)
+  }
+
+  const onUpdateNote = (updatedNote) => {
+    // 更新された新しいノートの配列を返す
+    const updatedNotesArray = notes.map((note) => {
+      if(note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    });
+
+    setNotes(updatedNotesArray);
+  }
+ 
+  return (
+    <div className='App'>
+      <Sidebar 
+        onAddNote={onAddNote} 
+        notes={notes} 
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+     />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote}/>
+    </div>
+  )
+}
+
+export default App
